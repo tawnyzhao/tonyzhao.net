f455f13edbfeebb83bb49d9b498b91850b4674c0
Tony Zhao <tawnyzhao@gmail.com>
2021-09-17 00:49:04 -0700

    Add homepage v1

--- src/App.js
+++ src/App.js
@@ -1,56 +1,77 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
-import './App.css';
-
+import {
+  chakra,
+  Box,
+  Heading,
+  Button,
+  ButtonGroup,
+  Center,
+  ChakraProvider,
+  Container,
+  SimpleGrid,
+  Stack,
+  Text,
+  Image,
+  Grid,
+  SlideFade,
+} from "@chakra-ui/react";
+import Profile from "./assets/profile.jpg";
+import React, { useState, useLayoutEffect } from "react";
 function App() {
-  const [date, setDate] = useState(null);
-  useEffect(() => {
-    async function getDate() {
-      const res = await fetch('/api/date');
-      const newDate = await res.text();
-      setDate(newDate);
-    }
-    getDate();
-  }, []);
   return (
-    <main>
-      <h1>Create React App + Go API</h1>
-      <h2>
-        Deployed with{' '}
-        <a
-          href="https://vercel.com/docs"
-          target="_blank"
-          rel="noreferrer noopener"
+    <ChakraProvider>
+      <Center h="100vh">
+        <Container
+          maxW={["container.sm", null, null, "container.xl"]}
+          py={4}
+          overflow="auto"
         >
-          Vercel
-        </a>
-        !
-      </h2>
-      <p>
-        <a
-          href="https://github.com/vercel/vercel/tree/main/examples/create-react-app"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          This project
-        </a>{' '}
-        was bootstrapped with{' '}
-        <a href="https://facebook.github.io/create-react-app/">
-          Create React App
-        </a>{' '}
-        and contains three directories, <code>/public</code> for static assets,{' '}
-        <code>/src</code> for components and content, and <code>/api</code>{' '}
-        which contains a serverless <a href="https://golang.org/">Go</a>{' '}
-        function. See{' '}
-        <a href="/api/date">
-          <code>api/date</code> for the Date API with Go
-        </a>
-        .
-      </p>
-      <br />
-      <h2>The date according to Go is:</h2>
-      <p>{date ? date : 'Loading date...'}</p>
-    </main>
+          <SimpleGrid columns={[1, null, null, 2]} spacing={8}>
+            <Stack
+              spacing={4}
+              justify="center"
+              align={["flex-start", null, null, "flex-end"]}
+            >
+              <Heading size="3xl">Hi I'm Tony!</Heading>
+              <Stack
+                wrap="wrap"
+                alignContent={["flex-start", null, null, "flex-end"]}
+                align={["flex-start", null, null, "flex-end"]}
+              >
+                <Text size="lg">
+                  I'm a software engineering student at the University of
+                  Waterloo.
+                </Text>
+                <Text size="lg">
+                  I'm currently working at
+                  <chakra.span fontWeight={700}> Applyboard </chakra.span>
+                  on the Platform team.
+                </Text>
+                <Text size="lg">
+                  Previously, I've worked at
+                  <chakra.span fontWeight={700}> LCBOnext</chakra.span> and
+                  <chakra.span fontWeight={700}> Condos.ca.</chakra.span>
+                </Text>
+              </Stack>
+              <Box>
+                <ButtonGroup>
+                  <Button variant="outline">Resume</Button>
+                  <Button>Github</Button>
+                </ButtonGroup>
+              </Box>
+            </Stack>
+            <Image
+              borderRadius="xl"
+              transition="all 0.25s"
+              _hover={{ transform: "scale(1.01, 1.01)" }}
+              boxSize={[80, null, null, "sm"]}
+              src={Profile}
+              alt={"Picture of Tony Zhao"}
+              objectFit="cover"
+            ></Image>
+          </SimpleGrid>
+        </Container>
+      </Center>
+    </ChakraProvider>
   );
 }
 
